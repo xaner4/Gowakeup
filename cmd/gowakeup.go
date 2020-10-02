8aca6d64c2713a7b394b12e631f13c2f60e9b691
Marius <marius-sol@pm.me>
2020-10-02 14:08:34 +0100

    Changes from flag lib to cobra for more flexebilitie

--- cmd/gowakeup.go
+++ cmd/gowakeup.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"gitlab.com/xaner4/gowakeup/pkg/wol"
+)
+
+var (
+	port int
+	ip   string
 )
 
 // Execute the Cobra CLI
@@ -14,11 +22,24 @@ func Execute() {
 	For many years people have printed back to the screen.`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			mp, err := wol.CreateMagicPacket(args[0])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 
+			err = wol.SendMagicPacket(mp, ip, port)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("Magic packet sent successfully to %q on port %d\n", ip, port)
+			}
 		},
 	}
 
-	var rootCmd = &cobra.Command{Use: "app"}
+	cmdWake.PersistentFlags().IntVarP(&port, "port", "p", 9, "Destination port")
+	cmdWake.PersistentFlags().StringVarP(&ip, "ip", "i", "255.255.255.255", "Destination IP address")
+	var rootCmd = &cobra.Command{Use: "gowakeup"}
 	rootCmd.AddCommand(cmdWake)
 	rootCmd.Execute()
 }
